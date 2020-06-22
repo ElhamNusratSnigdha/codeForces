d86c6cd0ad4ac1cf33bbc1be6f6971d2f6a124c7
Elham Nusrat <60064694+ElhamNusratSnigdha@users.noreply.github.com>
2020-06-22 16:36:43 +0600

    Add files via upload
    v2

--- NextRound.java
+++ NextRound.java
@@ -5,26 +5,26 @@ public class NextRound {
     public static void main(String[] args) {
         Scanner e = new Scanner(System.in);
 
+        int max = 0;
         String integer = e.nextLine();
         String[] split = integer.split(" ");
         int n = Integer.parseInt(split[0]);
         int k = Integer.parseInt(split[1]);;
-
-        String intArr = e.nextLine();
-        String[] splitA = intArr.split(" ");
-
-        int[] a = new int[splitA.length];
-        for (int i=0;i<a.length;i++){
-            a[i]=Integer.parseInt(splitA[i]);
+        String s = e.nextLine();
+        String[] sa = s.split(" ");
+        int[] a = new int[n];
+        for (int i=0;i<n;i++){
+            a[i]=Integer.parseInt(sa[i]);
+            if (i==(k-1)){
+                max=a[i];
+            }
         }
-
         int count=0;
-        for (int i=0;i<a.length;i++){
-            if(a[i]>k){
+        for (int i =0;i<n;i++){
+            if(a[i]!=0&&a[i]>=max){
                 count++;
             }
         }
-
         System.out.println(count);
     }
 }
